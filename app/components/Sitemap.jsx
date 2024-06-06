c8e47991e3085e8e4ed98e02c3216afea1ea6534
Luca G <47355690+Luc4G3r@users.noreply.github.com>
2024-06-06 16:19:03 +0200

    Create Sitemap.jsx
    Create component for sitemap data fetch

---
+++ app/components/Sitemap.jsx
@@ -0,0 +1,263 @@
+import {flattenConnection} from '@shopify/hydrogen';
+
+/**
+ * the google limit is 50K, however, the storefront API
+ * allows querying only 250 resources per pagination page
+ */
+const GRAPHQL_MAX_ENTRIES = 250;
+const SITEMAPS_LIMIT = 300;
+const MAX_URLS = 50000;
+
+var Sitemap = {
+  config: {
+    cursors: {
+      products: null,
+      collections: null,
+      pages: null,
+    },
+  },
+
+  async fetchProducts({storefront}) {
+    const query = SITEMAP_PRODUCT_QUERY;
+    const type = 'products';
+    return Sitemap.fetchAll({storefront, query, type});
+  },
+
+  async fetchCollections({storefront}) {
+    const query = SITEMAP_COLLECTION_QUERY;
+    const type = 'collections';
+    return Sitemap.fetchAll({storefront, query, type});
+  },
+
+  async fetchPages({storefront}) {
+    const query = SITEMAP_PAGE_QUERY;
+    const type = 'pages';
+    return Sitemap.fetchAll({storefront, query, type});
+  },
+
+  async fetchAll({storefront, query, type, depth}) {
+    let continueFetch = false;
+    const sitemapData = await storefront.query(query, {
+      variables: {
+        urlLimits: Sitemap.getQueryUrlLimit(),
+        language: storefront.i18n.language,
+        cursor: Sitemap.config.cursors[type],
+      },
+      cache: storefront.CacheLong(),
+    });
+
+    if (Object.prototype.hasOwnProperty.call(sitemapData[type], 'pageInfo')) {
+      if (
+        Object.prototype.hasOwnProperty.call(
+          sitemapData[type].pageInfo,
+          'hasNextPage',
+        )
+      ) {
+        continueFetch = sitemapData[type].pageInfo.hasNextPage;
+        Sitemap[type] = sitemapData[type].pageInfo.endCursor ?? null;
+      }
+    }
+
+    if (continueFetch && depth < SITEMAPS_LIMIT) {
+      depth = depth + 1;
+      let dataToMerge = await Sitemap.fetchProducts({storefront, depth});
+
+      if (dataToMerge && dataToMerge.nodes) {
+        dataToMerge.nodes.forEach(function (element) {
+          sitemapData[type].nodes.push(element);
+        });
+      }
+    }
+
+    return sitemapData[type];
+  },
+
+  generateSitemapUrls({data, baseUrl}) {
+    const products = flattenConnection(data.sitemaps.products)
+      .filter((product) => product.onlineStoreUrl)
+      .map((product) => {
+        const url = `${baseUrl}/products/${Sitemap.xmlEncode(product.handle)}`;
+
+        const productEntry = {
+          url,
+          lastMod: product.updatedAt,
+          changeFreq: 'daily',
+        };
+
+        if (product.featuredImage?.url) {
+          productEntry.image = {
+            url: Sitemap.xmlEncode(product.featuredImage.url),
+          };
+
+          if (product.title) {
+            productEntry.image.title = Sitemap.xmlEncode(product.title);
+          }
+
+          if (product.featuredImage.altText) {
+            productEntry.image.caption = Sitemap.xmlEncode(
+              product.featuredImage.altText,
+            );
+          }
+        }
+
+        return productEntry;
+      });
+
+    const collections = flattenConnection(data.sitemaps.collections)
+      .filter((collection) => collection.onlineStoreUrl)
+      .map((collection) => {
+        const url = `${baseUrl}/collections/${collection.handle}`;
+
+        return {
+          url,
+          lastMod: collection.updatedAt,
+          changeFreq: 'daily',
+        };
+      });
+
+    const pages = flattenConnection(data.sitemaps.pages)
+      .filter((page) => page.onlineStoreUrl)
+      .map((page) => {
+        const url = `${baseUrl}/pages/${page.handle}`;
+
+        return {
+          url,
+          lastMod: page.updatedAt,
+          changeFreq: 'weekly',
+        };
+      });
+
+    return [...products, ...collections, ...pages];
+  },
+
+  getMaxUrls() {
+    return MAX_URLS;
+  },
+
+  getQueryUrlLimit() {
+    if (GRAPHQL_MAX_ENTRIES < MAX_URLS) {
+      return GRAPHQL_MAX_ENTRIES;
+    }
+    return MAX_URLS;
+  },
+
+  xmlEncode(string) {
+    return string.replace(/[&<>'"]/g, (char) => `&#${char.charCodeAt(0)};`);
+  },
+
+  /**
+   * @param {Entry}
+   */
+  renderUrlTag({url, lastMod, changeFreq, image}) {
+    const imageTag = image
+      ? `<image:image>
+        <image:loc>${image.url}</image:loc>
+        <image:title>${image.title ?? ''}</image:title>
+        <image:caption>${image.caption ?? ''}</image:caption>
+      </image:image>`.trim()
+      : '';
+
+    return `
+    <url>
+      <loc>${url}</loc>
+      <lastmod>${lastMod}</lastmod>
+      <changefreq>${changeFreq}</changefreq>
+      ${imageTag}
+    </url>
+  `.trim();
+  },
+};
+
+export default Sitemap;
+
+/* aggregation queries are currently not supported by storefront API */
+// const SITEMAP_ENTRIES_COUNT_QUERY = `#graphql
+// query Sitemap($language: LanguageCode)
+// @inContext(language: $language) {
+//   aggregateProducts(
+//     query: "published_status:'online_store:visible'"
+//   ) {
+//     count
+//   }
+// }
+// `;
+
+const SITEMAP_PRODUCT_QUERY = `#graphql
+query Sitemap(
+  $urlLimits: Int
+  $language: LanguageCode
+  $cursor: String
+)
+@inContext(language: $language) {
+  products(
+    first: $urlLimits
+    after: $cursor
+    query: "published_status:'online_store:visible'"
+  ) {
+    nodes {
+      updatedAt
+      handle
+      onlineStoreUrl
+      title
+      featuredImage {
+        url
+        altText
+      }
+    }
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+  }
+}
+`;
+
+const SITEMAP_COLLECTION_QUERY = `#graphql
+query Sitemap(
+  $urlLimits: Int
+  $language: LanguageCode
+  $cursor: String
+)
+@inContext(language: $language) {
+  collections(
+    first: $urlLimits
+    after: $cursor
+    query: "published_status:'online_store:visible'"
+  ) {
+    nodes {
+      updatedAt
+      handle
+      onlineStoreUrl
+    }
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+  }
+}
+`;
+
+const SITEMAP_PAGE_QUERY = `#graphql
+query Sitemap(
+  $urlLimits: Int
+  $language: LanguageCode
+  $cursor: String
+)
+@inContext(language: $language) {
+  pages(
+    first: $urlLimits
+    after: $cursor
+    query: "published_status:'published'"
+  ) {
+    nodes {
+      updatedAt
+      handle
+      onlineStoreUrl
+    }
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+  }
+}
+`;
